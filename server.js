c1d0cc700435483e0fb430c0db375df487079bc2
sureshkumar-dev <sureshpackirisamy05@gmail.com>
2026-06-09 14:01:08 +0530

    new change 2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(cors({
-    origin: "https://farm-alert-website.vercel.app/",
+    origin: "https://farm-alert-website.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
